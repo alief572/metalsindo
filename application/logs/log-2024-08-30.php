29b533e1544c1c0e557ea842e79eb5881bf52f34
alief572 <146901652+alief572@users.noreply.github.com>
2024-08-30 13:35:59 +0700

    Dev - Module Metode Pembelian

--- application/logs/log-2024-08-30.php
+++ application/logs/log-2024-08-30.php
@@ -1,3 +1,405 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
 
 ERROR - 2024-08-30 04:23:37 --> 404 Page Not Found: /index
+ERROR - 2024-08-30 04:41:46 --> Severity: Warning --> mysqli::real_connect(): (HY000/1045): Access denied for user 'root'@'localhost' (using password: YES) C:\xampp56\htdocs\metalsindo\system\database\drivers\mysqli\mysqli_driver.php 202
+ERROR - 2024-08-30 04:41:46 --> Unable to connect to the database
+ERROR - 2024-08-30 09:42:09 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 09:43:21 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 04:45:06 --> 404 Page Not Found: ../modules/purchase_order_non_material/controllers//index
+ERROR - 2024-08-30 10:02:07 --> Query error: Table 'metalsindo_db.new_supplier' doesn't exist - Invalid query: 
+		SELECT 
+			a.*, 
+			b.nm_lengkap as nm_create, 
+			d.so_number,
+			f.no_pr as no_pr_material,
+			e.no_pr as no_pr_depart,
+			h.nama as nm_supplier,
+			IF(SUM(j.jumlahharga) IS NULL, 0, SUM(j.jumlahharga)) as harga_po
+		FROM 
+			tr_purchase_order as a 
+			LEFT JOIN users b ON b.id_user = a.created_by 
+			LEFT JOIN dt_trans_po c ON c.no_po = a.no_po 
+			LEFT JOIN material_planning_base_on_produksi_detail d ON d.id = c.idpr AND (c.tipe IS NULL OR c.tipe = '')
+			LEFT JOIN material_planning_base_on_produksi f ON f.so_number = d.so_number AND (c.tipe IS NULL OR c.tipe = '')
+			LEFT JOIN rutin_non_planning_detail e ON e.id = c.idpr AND c.tipe = 'pr depart'
+			LEFT JOIN rutin_non_planning_header g ON g.no_pengajuan = e.no_pengajuan
+			LEFT JOIN new_supplier h ON h.kode_supplier = a.id_suplier
+			LEFT JOIN dt_trans_po j ON j.no_po = a.no_po
+		WHERE
+			a.close_po IS NULL AND
+			(SELECT COUNT(aa.id) FROM dt_trans_po aa WHERE aa.no_po = a.no_po) > 0
+		GROUP BY a.no_po
+		ORDER BY a.no_po DESC
+	
+ERROR - 2024-08-30 10:02:16 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 10:14:53 --> Query error: Table 'metalsindo_db.master_suplier' doesn't exist - Invalid query: 
+		SELECT 
+			a.*, 
+			b.nm_lengkap as nm_create, 
+			d.so_number,
+			f.no_pr as no_pr_material,
+			e.no_pr as no_pr_depart,
+			h.name_suplier as nm_supplier,
+			IF(SUM(j.jumlahharga) IS NULL, 0, SUM(j.jumlahharga)) as harga_po
+		FROM 
+			tr_purchase_order_non_material as a 
+			LEFT JOIN users b ON b.id_user = a.created_by 
+			LEFT JOIN dt_trans_po_non_material c ON c.no_po = a.no_po 
+			LEFT JOIN material_planning_base_on_produksi_detail d ON d.id = c.idpr AND (c.tipe IS NULL OR c.tipe = '')
+			LEFT JOIN material_planning_base_on_produksi f ON f.so_number = d.so_number AND (c.tipe IS NULL OR c.tipe = '')
+			LEFT JOIN rutin_non_planning_detail e ON e.id = c.idpr AND c.tipe = 'pr depart'
+			LEFT JOIN rutin_non_planning_header g ON g.no_pengajuan = e.no_pengajuan
+			LEFT JOIN master_suplier h ON h.id_suplier = a.id_suplier
+			LEFT JOIN dt_trans_po_non_material j ON j.no_po = a.no_po
+		WHERE
+			a.close_po IS NULL AND
+			(SELECT COUNT(aa.id) FROM dt_trans_po_non_material aa WHERE aa.no_po = a.no_po) > 0
+		GROUP BY a.no_po
+		ORDER BY a.no_po DESC
+	
+ERROR - 2024-08-30 05:15:11 --> 404 Page Not Found: ../modules/purchase_order/controllers/Purchase_order/addPurchaseorder
+ERROR - 2024-08-30 10:16:44 --> Query error: Table 'metalsindo_db.customer' doesn't exist - Invalid query: SELECT *
+FROM `customer`
+ERROR - 2024-08-30 10:19:32 --> Query error: Table 'metalsindo_db.customer' doesn't exist - Invalid query: SELECT *
+FROM `customer`
+ERROR - 2024-08-30 10:19:53 --> Query error: Table 'metalsindo_db.tran_pr_header' doesn't exist - Invalid query: 
+			SELECT 
+				a.so_number as so_number,
+				a.no_pr as no_pr,
+				a.tgl_so as tgl_so, 
+				b.nm_lengkap as nama_user,
+				"pr material" as tipe_pr
+			FROM
+				material_planning_base_on_produksi a
+				LEFT JOIN users b ON b.id_user = a.booking_by
+			WHERE
+				(SELECT COUNT(x.id) as hitung FROM material_planning_base_on_produksi_detail x WHERE x.so_number = a.so_number ) > 0 AND 
+				a.metode_pembelian = "1" AND 
+				a.close_pr IS NULL
+
+			UNION ALL
+
+			SELECT 
+				a.no_pengajuan as so_number,
+				a.no_pr as no_pr,
+				DATE_FORMAT(a.created_date, "%Y-%m-%d") as tgl_so,
+				b.nm_lengkap as nama_user,
+				"pr depart" as tipe_pr
+			FROM
+				rutin_non_planning_header a
+				LEFT JOIN users b ON b.id_user = a.created_by
+			WHERE
+				a.sts_app = "Y" AND
+				a.metode_pembelian = "1" AND
+				a.close_pr IS NULL
+
+			UNION ALL
+
+			SELECT 
+				a.id as so_number,
+				a.no_pr as no_pr,
+				DATE_FORMAT(a.created_date, "%Y-%m-%d") as tgl_so,
+				b.nm_lengkap as nama_user,
+				"pr asset" as tipe_pr
+			FROM
+				tran_pr_header a
+				LEFT JOIN users b ON b.id_user = a.created_by
+			WHERE
+				a.app_status_3 = "Y" AND
+				a.metode_pembelian = "1" AND
+				a.close_pr IS NULL
+		
+ERROR - 2024-08-30 08:14:25 --> 404 Page Not Found: /index
+ERROR - 2024-08-30 08:14:29 --> 404 Page Not Found: /index
+ERROR - 2024-08-30 13:14:58 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 13:22:01 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 13:22:17 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 13:22:31 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 13:22:43 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 13:22:53 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 08:23:02 --> 404 Page Not Found: /index
+ERROR - 2024-08-30 08:23:36 --> 404 Page Not Found: ../modules/metode_pembelian/controllers/Metode_pembelian/index
+ERROR - 2024-08-30 08:24:05 --> 404 Page Not Found: ../modules/metode_pembelian/controllers/Metode_pembelian/index
+ERROR - 2024-08-30 08:24:16 --> Severity: Error --> Call to undefined function history() C:\xampp56\htdocs\metalsindo\application\modules\metode_pembelian\models\Metode_pembelian_model.php 22
+ERROR - 2024-08-30 08:24:20 --> 404 Page Not Found: ../modules/metode_pembelian/controllers/Metode_pembelian/index
+ERROR - 2024-08-30 13:24:25 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 13:24:56 --> Severity: Notice --> Undefined variable: datgroupmenu C:\xampp56\htdocs\metalsindo\application\modules\menus\views\menus_form.php 70
+ERROR - 2024-08-30 08:25:01 --> Severity: Error --> Call to undefined function history() C:\xampp56\htdocs\metalsindo\application\modules\metode_pembelian\models\Metode_pembelian_model.php 22
+ERROR - 2024-08-30 08:27:23 --> Query error: Table 'metalsindo_db.tran_pr_detail' doesn't exist - Invalid query: SELECT *
+FROM `tran_pr_detail`
+WHERE `no_pr_group` = 'PRN24080001'
+ERROR - 2024-08-30 08:27:29 --> Query error: Table 'metalsindo_db.tran_pr_detail' doesn't exist - Invalid query: SELECT *
+FROM `tran_pr_detail`
+WHERE `no_pr_group` = 'PRN24080001'
+ERROR - 2024-08-30 08:28:28 --> Query error: Table 'metalsindo_db.tran_pr_detail' doesn't exist - Invalid query: SELECT *
+FROM `tran_pr_detail`
+WHERE `no_pr_group` = 'PRN24080001'
+ERROR - 2024-08-30 08:28:45 --> Query error: Table 'metalsindo_db.tran_pr_detail' doesn't exist - Invalid query: SELECT *
+FROM `tran_pr_detail`
+WHERE `no_pr_group` = 'PRN24080001'
+ERROR - 2024-08-30 08:28:51 --> Query error: Table 'metalsindo_db.new_supplier' doesn't exist - Invalid query: SELECT kode_supplier AS id_supplier, nama AS nm_supplier FROM new_supplier ORDER BY nm_supplier ASC 
+ERROR - 2024-08-30 08:29:55 --> Query error: Table 'metalsindo_db.tran_pr_header' doesn't exist - Invalid query: 
+				SELECT
+					a.no_pr as no_pr,
+					a.tgl_so as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					IF(a.category = "pr stok", "stok", "material") as category,
+					a.so_number as so_number
+				FROM
+					material_planning_base_on_produksi a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+					a.category IN ("pr material", "pr stok") AND
+					a.close_pr IS NULL AND
+					a.metode_pembelian IS NULL AND (
+						a.no_pr LIKE "%%" OR
+						a.tgl_so LIKE "%%" OR
+						b.nm_lengkap LIKE "%%" OR
+						a.created_date LIKE "%%"
+					)
+				
+				UNION ALL
+	
+				SELECT
+					a.no_pr as no_pr,
+					a.created_date as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					"departemen" as category,
+					"" as so_number
+				FROM
+					rutin_non_planning_header a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+						a.metode_pembelian IS NULL AND
+						a.close_pr IS NULL AND
+						(
+							a.no_pr LIKE "%%" OR
+							a.created_date LIKE "%%" OR
+							b.nm_lengkap LIKE "%%"
+						)
+				
+				UNION ALL
+
+				SELECT
+					a.no_pr as no_pr,
+					a.created_date as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					"asset" as category,
+					"" as so_number
+				FROM
+					tran_pr_header a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+					a.metode_pembelian IS NULL AND
+					a.close_pr IS NULL AND
+					(
+						a.no_pr LIKE "%%" OR
+						a.created_date LIKE "%%" OR
+						b.nm_lengkap LIKE "%%"
+					)
+
+
+			
+ERROR - 2024-08-30 08:30:24 --> Query error: Table 'metalsindo_db.tran_pr_header' doesn't exist - Invalid query: 
+				SELECT
+					a.no_pr as no_pr,
+					a.tgl_so as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					IF(a.category = "pr stok", "stok", "material") as category,
+					a.so_number as so_number
+				FROM
+					material_planning_base_on_produksi a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+					a.category IN ("pr material", "pr stok") AND
+					a.close_pr IS NULL AND
+					a.metode_pembelian IS NULL AND (
+						a.no_pr LIKE "%%" OR
+						a.tgl_so LIKE "%%" OR
+						b.nm_lengkap LIKE "%%" OR
+						a.created_date LIKE "%%"
+					)
+				
+				UNION ALL
+	
+				SELECT
+					a.no_pr as no_pr,
+					a.created_date as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					"departemen" as category,
+					"" as so_number
+				FROM
+					rutin_non_planning_header a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+						a.metode_pembelian IS NULL AND
+						a.close_pr IS NULL AND
+						(
+							a.no_pr LIKE "%%" OR
+							a.created_date LIKE "%%" OR
+							b.nm_lengkap LIKE "%%"
+						)
+				
+				UNION ALL
+
+				SELECT
+					a.no_pr as no_pr,
+					a.created_date as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					"asset" as category,
+					"" as so_number
+				FROM
+					tran_pr_header a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+					a.metode_pembelian IS NULL AND
+					a.close_pr IS NULL AND
+					(
+						a.no_pr LIKE "%%" OR
+						a.created_date LIKE "%%" OR
+						b.nm_lengkap LIKE "%%"
+					)
+
+
+			
+ERROR - 2024-08-30 08:31:57 --> Query error: Table 'metalsindo_db.tran_pr_header' doesn't exist - Invalid query: 
+				SELECT
+					a.no_pr as no_pr,
+					a.tgl_so as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					IF(a.category = "pr stok", "stok", "material") as category,
+					a.so_number as so_number
+				FROM
+					material_planning_base_on_produksi a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+					a.category IN ("pr material", "pr stok") AND
+					a.close_pr IS NULL AND
+					a.metode_pembelian IS NULL AND (
+						a.no_pr LIKE "%%" OR
+						a.tgl_so LIKE "%%" OR
+						b.nm_lengkap LIKE "%%" OR
+						a.created_date LIKE "%%"
+					)
+				
+				UNION ALL
+	
+				SELECT
+					a.no_pr as no_pr,
+					a.created_date as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					"departemen" as category,
+					"" as so_number
+				FROM
+					rutin_non_planning_header a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+						a.metode_pembelian IS NULL AND
+						a.close_pr IS NULL AND
+						(
+							a.no_pr LIKE "%%" OR
+							a.created_date LIKE "%%" OR
+							b.nm_lengkap LIKE "%%"
+						)
+				
+				UNION ALL
+
+				SELECT
+					a.no_pr as no_pr,
+					a.created_date as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					"asset" as category,
+					"" as so_number
+				FROM
+					tran_pr_header a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+					a.metode_pembelian IS NULL AND
+					a.close_pr IS NULL AND
+					(
+						a.no_pr LIKE "%%" OR
+						a.created_date LIKE "%%" OR
+						b.nm_lengkap LIKE "%%"
+					)
+
+
+			
+ERROR - 2024-08-30 08:32:10 --> Query error: Table 'metalsindo_db.tran_pr_header' doesn't exist - Invalid query: 
+				SELECT
+					a.no_pr as no_pr,
+					a.tgl_so as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					IF(a.category = "pr stok", "stok", "material") as category,
+					a.so_number as so_number
+				FROM
+					material_planning_base_on_produksi a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+					a.category IN ("pr material", "pr stok") AND
+					a.close_pr IS NULL AND
+					a.metode_pembelian IS NULL AND (
+						a.no_pr LIKE "%%" OR
+						a.tgl_so LIKE "%%" OR
+						b.nm_lengkap LIKE "%%" OR
+						a.created_date LIKE "%%"
+					)
+				
+				UNION ALL
+	
+				SELECT
+					a.no_pr as no_pr,
+					a.created_date as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					"departemen" as category,
+					"" as so_number
+				FROM
+					rutin_non_planning_header a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+						a.metode_pembelian IS NULL AND
+						a.close_pr IS NULL AND
+						(
+							a.no_pr LIKE "%%" OR
+							a.created_date LIKE "%%" OR
+							b.nm_lengkap LIKE "%%"
+						)
+				
+				UNION ALL
+
+				SELECT
+					a.no_pr as no_pr,
+					a.created_date as tgl_pr,
+					b.nm_lengkap as request_by,
+					a.created_date as request_date,
+					"asset" as category,
+					"" as so_number
+				FROM
+					tran_pr_header a
+					LEFT JOIN users b ON b.id_user = a.created_by
+				WHERE
+					a.metode_pembelian IS NULL AND
+					a.close_pr IS NULL AND
+					(
+						a.no_pr LIKE "%%" OR
+						a.created_date LIKE "%%" OR
+						b.nm_lengkap LIKE "%%"
+					)
+
+
+			
+ERROR - 2024-08-30 13:34:06 --> Query error: Table 'metalsindo_db.so_internal' doesn't exist - Invalid query: SELECT `a`.*, `b`.`due_date`, `c`.`nm_customer`
+FROM `material_planning_base_on_produksi` `a`
+LEFT JOIN `so_internal` `b` ON `a`.`so_number`=`b`.`so_number`
+LEFT JOIN `customer` `c` ON `a`.`id_customer`=`c`.`id_customer`
+WHERE `a`.`so_number` = 'P240800001'
--- application/modules/purchase_order_non_material/views/index.php
+++ application/modules/purchase_order_non_material/views/index.php
@@ -18,7 +18,7 @@ $ENABLE_DELETE  = has_permission('Purchase_Order.Delete');
 	<div class="box-header">
 		<span class="pull-left">
 			<?php if ($ENABLE_ADD) : ?>
-				<a class="btn btn-success btn-sm" href="<?= base_url('/purchase_order/add/' . $record->no_penawaran) ?>" title="Add"><i class="fa fa-plus"></i>&nbsp;Create</a>
+				<a class="btn btn-success btn-sm" href="<?= base_url('/purchase_order_non_material/addPurchaseorder/') ?>" title="Add"><i class="fa fa-plus"></i>&nbsp;Create PO</a>
 			<?php endif; ?>
 		</span>
 	</div>
@@ -30,9 +30,15 @@ $ENABLE_DELETE  = has_permission('Purchase_Order.Delete');
 				<tr>
 					<th width="5">#</th>
 					<th>No PO</th>
+					<th>No PR</th>
+					<th>No Incoming</th>
 					<th>Tanggal PO</th>
-					<th>Supplier</th>
-					<th>Progress PR</th>
+					<th>Progress PO</th>
+					<th>PO</th>
+					<th>Vendor</th>
+					<th>Harga PO</th>
+					<th>Revisi</th>
+					<th>Reject Reason</th>
 					<?php if ($ENABLE_MANAGE) : ?>
 						<th>Action</th>
 					<?php endif; ?>
@@ -45,38 +51,118 @@ $ENABLE_DELETE  = has_permission('Purchase_Order.Delete');
 
 					$numb = 0;
 					foreach ($results as $record) {
-						$numb++; ?>
+						$valid_edit = 1;
+						$numb++;
+
+						$no_pr = [];
+						$get_no_pr = $this->db->query("
+							SELECT
+								b.no_pr as no_pr
+							FROM
+								material_planning_base_on_produksi_detail a
+								JOIN material_planning_base_on_produksi b ON b.so_number = a.so_number
+							WHERE
+								a.id IN (SELECT aa.idpr FROM dt_trans_po aa WHERE aa.no_po = '" . $record->no_po . "' AND (aa.tipe IS NULL OR aa.tipe = ''))
+							GROUP BY b.no_pr
+
+							UNION ALL 
+
+							SELECT
+								b.no_pr as no_pr
+							FROM
+								rutin_non_planning_detail a
+								JOIN rutin_non_planning_header b ON b.no_pengajuan = a.no_pengajuan
+							WHERE
+								a.id IN (SELECT aa.idpr FROM dt_trans_po aa WHERE aa.no_po = '" . $record->no_po . "' AND aa.tipe = 'pr depart')
+							GROUP BY b.no_pr
+						")->result();
+						foreach ($get_no_pr as $item_pr) {
+							$no_pr[] = $item_pr->no_pr;
+						}
+
+						$no_pr = implode(', ', $no_pr);
+
+				?>
 						<tr>
 							<td><?= $numb; ?></td>
 							<td><?= $record->no_surat ?></td>
+							<td><?= $no_pr ?></td>
+							<td><?= $list_no_incoming[$record->no_po] ?></td>
 							<td><?= date('d-M-Y', strtotime($record->tanggal)) ?></td>
-							<td><?= $record->namesup ?></td>
-							<?php if ($record->status == '1') {
-								echo "<td><span class='badge bg-blue'>Waiting</span></td>";
+							<?php
+							$ttl_qty_incoming = 0;
+							if ($record->status == '1') {
+								if ($record->reject_reason !== null && $record->reject_reason !== '') {
+									echo "<td class='text-center'><span class='badge bg-red'>Reject</span></td>";
+								} else {
+									echo "<td class='text-center'><span class='badge bg-blue'>Waiting</span></td>";
+								}
 							} elseif ($record->status == '2') {
-								echo "<td><span class='badge bg-green'>Approved</span></td>";
+								echo "<td class='text-center'><span class='badge bg-green'>Approved</span></td>";
 							} else {
-								echo "<td><span class='badge bg-red'>Closed</span></td>";
+								echo "<td class='text-center'><span class='badge bg-red'>Closed</span></td>";
+							}
+
+							if ($record->status == '1') {
+								echo "<td class='text-center'><span class='badge bg-red'>Waiting Approval</span></td>";
+							} else {
+								$sts = "<td class='text-center'><span class='badge bg-orange'>Waiting In</span></td>";
+
+								$get_qty_incoming = $this->db->select('IF(SUM(qty_oke) IS NUll, 0, SUM(qty_oke)) as ttl_qty_incoming')->get_where('tr_checked_incoming_detail', ['no_ipp' => $record->no_po])->row();
+								$ttl_qty_incoming = $get_qty_incoming->ttl_qty_incoming;
+
+								$get_ttl_qty_po = $this->db->select('IF(SUM(qty) IS NULL, 0, SUM(qty)) AS ttl_qty_po')->get_where('dt_trans_po', ['no_po' => $record->no_po])->row();
+								$ttl_qty_po = $get_ttl_qty_po->ttl_qty_po;
+
+								if ($ttl_qty_incoming < 1) {
+									$ttl_qty_incoming = 0;
+									$this->db->select('b.qty_oke');
+									$this->db->from('warehouse_adjustment a');
+									$this->db->join('warehouse_adjustment_detail b', 'b.kode_trans = a.kode_trans');
+									$this->db->where('a.no_ipp', $record->no_po);
+									$get_ttl_incoming = $this->db->get()->result();
+
+									foreach ($get_ttl_incoming as $item_ttl_incoming) {
+										$ttl_qty_incoming += $item_ttl_incoming->qty_oke;
+									}
+								}
+
+								if ($ttl_qty_incoming > 0) {
+									$valid_edit = 0;
+									if ($ttl_qty_incoming >= $ttl_qty_po) {
+										$sts = "<td class='text-center'><span class='badge bg-green'>Complete</span></td>";
+									} else {
+
+										$sts = "<td class='text-center'><span class='badge bg-purple'>Partial</span></td>";
+									}
+								}
+
+								echo $sts;
 							}
 							?>
-
+							<td><?= $record->nm_supplier ?></td>
+							<td class="text-right"><?= number_format($record->total_barang - $record->nilai_disc + $record->total_ppn + $record->taxtotal) ?></td>
+							<td class="text-center"><?= $record->revisi ?></td>
+							<td><?= $record->reject_reason ?></td>
 							<td style="padding-left:20px">
 								<?php if ($ENABLE_VIEW) : ?>
-									<a class="btn btn-warning btn-sm view" href="javascript:void(0)" title="View" data-no_po="<?= $record->no_po ?>"><i class="fa fa-eye"></i>
-									</a>
+									<a class="btn btn-warning btn-sm" href="<?= base_url('/purchase_order/view_po/' . $record->no_po) ?>" title="View" data-no_po="<?= $record->no_po ?>"><i class="fa fa-eye"></i></a>
+
 									<a class="btn btn-primary btn-sm" href="<?= base_url('/purchase_order/PrintH2/' . $record->no_po) ?>" target="_blank" title="Print"><i class="fa fa-print"></i></a>
 								<?php endif; ?>
-								<?php if ($ENABLE_MANAGE) :
-									if ($record->status == '1') { ?>
-										<a class="btn btn-info btn-sm" href="<?= base_url('/purchase_order/edit/' . $record->no_po) ?>" title="Edit"><i class="fa fa-edit"></i></a>
-								<?php }
+								<?php if ($ENABLE_MANAGE && $valid_edit > 0) :
+								?>
+									<a class="btn btn-info btn-sm" href="<?= base_url('/purchase_order/edit/' . $record->no_po) ?>" title="Edit"><i class="fa fa-edit"></i></a>
+
+								<?php
 								endif; ?>
-								<?php if ($ENABLE_MANAGE) :
-									if ($record->status == '1') { ?>
-										<a class="btn btn-success btn-sm Approve" href="javascript:void(0)" title="Approval PO" data-no_po="<?= $record->no_po ?>"><i class="fa fa-check"></i>
-										</a>
-								<?php }
-								endif; ?>
+
+								<?php
+								if ($ENABLE_DELETE) {
+									echo '<button type="button" class="btn btn-sm btn-danger close_po_modal" data-no_po="' . $record->no_po . '" title="Close PO"><i class="fa fa-close"></i></button>';
+								}
+								?>
+
 							</td>
 
 						</tr>
@@ -126,6 +212,28 @@ $ENABLE_DELETE  = has_permission('Purchase_Order.Delete');
 	</div>
 </div>
 
+<div class="modal modal-default fade" id="dialog-popupCP" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+	<div class="modal-dialog modal-md">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+				<h4 class="modal-title" id="myModalLabel">Closing PO</h4>
+			</div>
+			<form action="" method="post" id="CP-frm-data">
+				<div class="modal-body" id="ModalViewCP">
+					...
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">
+						<span class="glyphicon glyphicon-remove"></span> Cancel
+					</button>
+					<button type="submit" class="btn btn-sm btn-danger">Close It!</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
+
 <!-- DataTables -->
 <script src="<?= base_url('assets/plugins/datatables/jquery.dataTables.min.js') ?>"></script>
 <script src="<?= base_url('assets/plugins/datatables/dataTables.bootstrap.min.js') ?>"></script>
@@ -133,6 +241,70 @@ $ENABLE_DELETE  = has_permission('Purchase_Order.Delete');
 <script src="<?= base_url('assets/js/autoNumeric.js') ?>"></script>
 <!-- page script -->
 <script type="text/javascript">
+	$(document).ready(function() {
+		$('#example1').dataTable();
+	});
+
+	$(document).on('click', '.close_po_modal', function() {
+		var no_po = $(this).data('no_po');
+
+		$.ajax({
+			type: 'post',
+			url: siteurl + active_controller + 'close_po_modal',
+			data: {
+				'no_po': no_po
+			},
+			cache: false,
+			success: function(result) {
+				$('#ModalViewCP').html(result);
+				$('#dialog-popupCP').modal('show');
+			},
+			error: function(result) {
+				swal({
+					title: 'Error !',
+					text: 'Please try again later !',
+					type: 'error'
+				});
+			}
+		});
+	});
+
+	$(document).on('submit', '#CP-frm-data', function() {
+		var data = new FormData($('#CP-frm-data')[0]);
+
+		$.ajax({
+			type: 'post',
+			url: siteurl + active_controller + 'close_po',
+			data: data,
+			cache: false,
+			dataType: 'json',
+			processData: false,
+			contentType: false,
+			success: function(result) {
+				if (result.status == 1) {
+					swal({
+						title: 'Success !',
+						text: 'PO has been closed',
+						type: 'success'
+					});
+				} else {
+					swal({
+						title: 'Failed !',
+						text: 'PO has not been closed',
+						type: 'warning'
+					});
+				}
+			},
+			error: function(result) {
+				swal({
+					title: 'Error !',
+					text: 'Please try again later !',
+					type: 'error'
+				});
+			}
+		});
+	});
+
 	$(document).on('click', '.edit', function(e) {
 		var id = $(this).data('no_penawaran');
 		$("#head_title").html("<i class='fa fa-list-alt'></i><b>Edit Inventory</b>");
@@ -197,7 +369,7 @@ $ENABLE_DELETE  = has_permission('Purchase_Order.Delete');
 		// alert(id);
 		swal({
 				title: "Anda Yakin?",
-				text: "P.R. Akan Dihapus.",
+				text: "PO. Akan Di Approve.",
 				type: "warning",
 				showCancelButton: true,
 				confirmButtonClass: "btn-info",
@@ -270,10 +442,7 @@ $ENABLE_DELETE  = has_permission('Purchase_Order.Delete');
 		// } );
 		// } );
 
-		var table = $('#example1').DataTable({
-			orderCellsTop: true,
-			fixedHeader: true
-		});
+
 		$("#form-area").hide();
 	});
 
