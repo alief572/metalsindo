0d8616b375fe229dbb79a037817c0ba088a1b32c
Alief Daffa <aliefdaffa572@gmail.com>
2024-11-15 15:26:53 +0700

    Perbaikan - Pengajuan Pembayaran Periodik

--- application/modules/pengajuan_rutin/controllers/Pengajuan_rutin.php
+++ application/modules/pengajuan_rutin/controllers/Pengajuan_rutin.php
@@ -65,7 +65,7 @@ class Pengajuan_rutin extends Admin_Controller
 		$datdept  = $this->All_model->GetDeptCombo();
 		$this->template->set('datdept', $datdept);
 		$this->template->set('results', $data);
-		$this->template->title('Pengajuan Pembayaran Periodik');
+		$this->template->title('Approval Pengajuan Pembayaran Periodik');
 		$this->template->render('app_list');
 	}
 
