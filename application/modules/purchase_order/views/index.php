f5845d9d413127ba345868406629d09d3846ef41
Alief Daffa <aliefdaffa572@gmail.com>
2024-11-18 14:48:32 +0700

    Perbaikan - Purchase Order
    tombol print dimunculkan meski PO belum di approve

--- application/modules/purchase_order/views/index.php
+++ application/modules/purchase_order/views/index.php
@@ -64,7 +64,7 @@ $ENABLE_DELETE  = has_permission('Purchase_Order.Delete');
 								<?php if ($ENABLE_VIEW) : ?>
 									<a class="btn btn-warning btn-sm view" href="javascript:void(0)" title="View" data-no_po="<?= $record->no_po ?>"><i class="fa fa-eye"></i>
 									</a>
-									<?php endif; ?>
+								<?php endif; ?>
 								<?php if ($ENABLE_MANAGE) :
 									if ($record->status == '1') { ?>
 										<a class="btn btn-info btn-sm" href="<?= base_url('/purchase_order/edit/' . $record->no_po) ?>" title="Edit"><i class="fa fa-edit"></i></a>
@@ -77,10 +77,11 @@ $ENABLE_DELETE  = has_permission('Purchase_Order.Delete');
 								<?php }
 								endif; ?>
 								<?php if ($ENABLE_MANAGE) :
-									if ($record->status == '2') { ?>
-										<a class="btn btn-primary btn-sm" href="<?= base_url('/purchase_order/PrintH2/' . $record->no_po) ?>" target="_blank" title="Print"><i class="fa fa-print"></i></a>
-								<?php }
+									// if ($record->status == '2') {
+									// }
 								endif; ?>
+
+								<a class="btn btn-primary btn-sm" href="<?= base_url('/purchase_order/PrintH2/' . $record->no_po) ?>" target="_blank" title="Print"><i class="fa fa-print"></i></a>
 							</td>
 
 						</tr>
