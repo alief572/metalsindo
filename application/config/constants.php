40f033b0329247faa38dcfe1836d5847dad67a12
hikmat20 <hikmataulia20@gmail.com>
2025-02-24 15:35:34 +0700

    awd

--- application/config/constants.php
+++ application/config/constants.php
@@ -84,5 +84,5 @@ defined('EXIT_DATABASE')       OR define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      OR define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      OR define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-define('DBACC', 'gl_metalsindo_live');
-define('DBERP', 'metalsindo_live');
+define('DBACC', 'gl_metalsindo_dev');
+define('DBERP', 'metalsindo_dev');
