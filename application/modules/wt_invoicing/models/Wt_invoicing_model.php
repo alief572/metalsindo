8fd0e9bdfb5ac220ca7dbcbed022cda604047393
alief572 <146901652+alief572@users.noreply.github.com>
2025-09-09 10:37:22 +0700

    Perbaikan - Delivery, PO, PR, Penawaran dan SPK Marketing
    untuk generator id nya tidak tarik dari nomor yang paling baru

--- application/modules/wt_invoicing/models/Wt_invoicing_model.php
+++ application/modules/wt_invoicing/models/Wt_invoicing_model.php
@@ -150,11 +150,11 @@ class Wt_invoicing_model extends BF_Model
       $romawi = 'XII';
     }
     $blnthn = date('Y-m');
-    $query = $this->db->query("SELECT MAX(no_surat) as max_id FROM tr_invoice WHERE no_surat LIKE '%/" . date('y', strtotime($th)) . "/" . $romawi . "/%'");
+    $query = $this->db->query("SELECT MAX(RIGHT(no_surat, 4)) as max_id FROM tr_invoice WHERE no_surat LIKE '%/" . date('y', strtotime($th)) . "/" . $romawi . "/%'");
     $row = $query->row_array();
     $thn = date('T');
     $max_id = $row['max_id'];
-    $max_id1 = (int) substr($max_id, -4);
+    $max_id1 = (int) $max_id;
     $counter = $max_id1 + 1;
     $idcust = "INV-MP/" . $tahun . "/" . $romawi . "/" . sprintf("%04s", $counter);
     return $idcust;
