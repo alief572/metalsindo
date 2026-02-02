9490b69ff4d0ecbe2c60d0bf3b237d1fad2febdc
Alief Daffa <aliefdaffa572@gmail.com>
2026-02-02 16:00:35 +0700

    awd

--- application/modules/transaksi_inquiry/views/editinquiry.php
+++ application/modules/transaksi_inquiry/views/editinquiry.php
@@ -1,300 +1,301 @@
 <?php
-    $ENABLE_ADD     = has_permission('Trans_inquiry.Add');
-    $ENABLE_MANAGE  = has_permission('Trans_inquiry.Manage');
-    $ENABLE_VIEW    = has_permission('Trans_inquiry.View');
-    $ENABLE_DELETE  = has_permission('Trans_inquiry.Delete');
-	$tanggal = date('Y-m-d');
-	foreach ($results['head'] as $head){
-}	
+$ENABLE_ADD     = has_permission('Trans_inquiry.Add');
+$ENABLE_MANAGE  = has_permission('Trans_inquiry.Manage');
+$ENABLE_VIEW    = has_permission('Trans_inquiry.View');
+$ENABLE_DELETE  = has_permission('Trans_inquiry.Delete');
+$tanggal = date('Y-m-d');
+foreach ($results['head'] as $head) {
+}
 ?>
 
- <div class="box box-primary">
-    <div class="box-body">
+<div class="box box-primary">
+	<div class="box-body">
 		<form id="data-form" method="post">
 			<div class="col-sm-12">
 				<div class="input_fields_wrap2">
-			<div class="row">
-		<center><label for="customer" ><h3>INQUIRY (CUSTOMER REQUIREMENTS CHECK LIST)</h3></label></center>
-		<div class="col-sm-12">
-		<div class="col-sm-6">
-		<div class="form-group row">
-			<div class="col-md-4">
-				<label for="customer">NO.CRCL</label>
-			</div>
-			<div class="col-md-8">
-				<input type="text" class="form-control" id="no_inquiry"  required name="no_inquiry" readonly value="<?= $head->no_inquiry ?>" placeholder="No.CRCL">
-			</div>
-		</div>
-		</div>
-		<div class="col-sm-6">
-		<div class="form-group row">
-			<div class="col-md-4">
-				<label for="customer">Tanggal</label>
-			</div>
-			<div class="col-md-8">
-				<input type="date" class="form-control" id="tanggal" value="<?= $head->tgl_inquiry ?>" onkeyup required name="tanggal" readonly >
-			</div>
-		</div>
-		</div>
-		</div>
-		<div class="col-sm-12">
-		<div class="col-sm-6">
-			<div class="form-group row">
-				<div class="col-md-4">
-					<label for="id_category_supplier">SALES/MARKETING</label>
+					<div class="row">
+						<center><label for="customer">
+								<h3>INQUIRY (CUSTOMER REQUIREMENTS CHECK LIST)</h3>
+							</label></center>
+						<div class="col-sm-12">
+							<div class="col-sm-6">
+								<div class="form-group row">
+									<div class="col-md-4">
+										<label for="customer">NO.CRCL</label>
+									</div>
+									<div class="col-md-8">
+										<input type="text" class="form-control" id="no_inquiry" required name="no_inquiry" readonly value="<?= $head->no_inquiry ?>" placeholder="No.CRCL">
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="form-group row">
+									<div class="col-md-4">
+										<label for="customer">Tanggal</label>
+									</div>
+									<div class="col-md-8">
+										<input type="date" class="form-control" id="tanggal" value="<?= $head->tgl_inquiry ?>" onkeyup required name="tanggal" readonly>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="col-sm-6">
+								<div class="form-group row">
+									<div class="col-md-4">
+										<label for="id_category_supplier">SALES/MARKETING</label>
+									</div>
+									<div class="col-md-8">
+										<select id="id_sales" name="id_sales" class="form-control select" required>
+											<option value="">--Pilih--</option>
+											<?php foreach ($results['karyawan'] as $karyawan) {
+
+												if ($karyawan->id_karyawan == $head->id_sales) {
+													$selected = 'selected';
+												} else {
+													$selected = '';
+												}
+
+											?>
+
+												<option value="<?= $karyawan->id_karyawan ?>" <?= $selected ?>><?= strtoupper(strtolower($karyawan->nama_karyawan)) ?></option>
+											<?php } ?>
+										</select>
+									</div>
+								</div>
+							</div>
+							<div class="col-sm-6">
+								<div class="form-group row">
+									<div class="col-md-4">
+										<label for="id_customer">CUSTOMER</label>
+									</div>
+									<div class="col-md-8">
+										<select id="id_customer" name="id_customer" class="form-control select" onchange="get_customer()" required>
+											<option value="">--Pilih--</option>
+											<?php foreach ($results['customers'] as $customers) {
+												if ($customers->id_customer == $head->id_customer) {
+													$selected = 'selected';
+												} else {
+													$selected = '';
+												}
+											?>
+												<option value="<?= $customers->id_customer ?>" <?= $selected ?>><?= strtoupper(strtolower($customers->name_customer)) ?></option>
+											<?php } ?>
+										</select>
+									</div>
+								</div>
+							</div>
+						</div>
+						</br>
+						<div class="col-sm-12">
+							<div class="form-group row">
+								<div class="col-md-12" id='data_customer'>
+									<div class='col-sm-6'>
+										<div class='form-group row'>
+											<div class='col-md-4'>
+												<label for='email_customer'>EMAIL</label>
+											</div>
+											<div class='col-md-8'>
+												<input type='email' class='form-control' id='email_customer' required name='email_customer' value="<?= $head->email_customer ?>">
+											</div>
+										</div>
+									</div>
+									<div class='col-sm-6'>
+										<div class='form-group row'>
+											<div class='col-md-4'>
+												<label for='id_category_supplier'>PIC CUSTOMER</label>
+											</div>
+											<div class='col-md-8'>
+												<input type='text' class='form-control' id='pic_customer' required name='pic_customer' value="<?= $head->pic_customer ?>">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="form-group row">
+								<div class="col-md-12" id='data_customer'>
+									<div class='col-sm-6'>
+										<div class='form-group row'>
+											<div class='col-md-4'>
+												<label for='email_customer'>Berat Maksimum/palet</label>
+											</div>
+											<div class='col-md-8'>
+												<input type='nember' class='form-control' id='berat_palet' required name='berat_palet' value="<?= $head->berat_palet ?>">
+											</div>
+										</div>
+									</div>
+									<div class='col-sm-6'>
+										<div class='form-group row'>
+											<div class='col-md-4'>
+												<label for='id_category_supplier'>Tinggi Maksimum/palet</label>
+											</div>
+											<div class='col-md-8'>
+												<input type='number' class='form-control' id='tinggi_palet' required name='tinggi_palet' value="<?= $head->tinggi_palet ?>">
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-sm-12">
+							<div class="form-group row">
+								<div class="col-md-12" id='data_customer'>
+									<div class='col-sm-6'>
+										<div class='form-group row'>
+											<div class='col-md-4'>
+												<label for='doc_crcl'>Upload Document CRCL</label>
+											</div>
+											<div class='col-md-8'>
+												<input type='file' class='form-control' id='doc_crcl' required name='doc_crcl'>
+											</div>
+										</div>
+									</div>
+									<div class='col-sm-6' hidden>
+										<div class='form-group row'>
+											<div class='col-md-4'>
+												<label for='id_category_supplier'></label>
+											</div>
+											<div class='col-md-8'>
+												<input type='number' class='form-control' id='tinggi_palet2' required name='tinggi_palet2'>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+					<center>
+						<button type="submit" class="btn btn-success btn-sm" name="save" id="simpan-com"><i class="fa fa-save"></i>Simpan</button>
+					</center>
 				</div>
-				<div class="col-md-8">
-					<select id="id_sales" name="id_sales" class="form-control select" required>
-						<option value="">--Pilih--</option>
-							<?php foreach ($results['karyawan'] as $karyawan){
-								
-								if( $karyawan->id_karyawan == $head->id_sales){									
-									$selected ='selected';		
-								}else{
-									$selected ='';
-								}
-								
-								?>
-							
-						<option value="<?= $karyawan->id_karyawan?>" <?= $selected ?> ><?= strtoupper(strtolower($karyawan->nama_karyawan))?></option>
-							<?php } ?>
-					</select>
-				</div>
 			</div>
-		</div>
-		<div class="col-sm-6">
-			<div class="form-group row">
-				<div class="col-md-4">
-					<label for="id_customer">CUSTOMER</label>
-				</div>
-				<div class="col-md-8">
-					<select id="id_customer" name="id_customer" class="form-control select" onchange="get_customer()" required>
-						<option value="">--Pilih--</option>
-							<?php foreach ($results['customers'] as $customers){
-								if( $customers->id_customer == $head->id_customer){									
-									$selected ='selected';		
-								}else{
-									$selected ='';
-								}
-							?>
-						<option value="<?= $customers->id_customer?>" <?= $selected ?>><?= strtoupper(strtolower($customers->name_customer))?></option>
-							<?php } ?>
-					</select>
-				</div>
-			</div>
-		</div>
-		</div>
-		</br>
-		<div class="col-sm-12">
-		<div class="form-group row" >
-		<div class="col-md-12" id='data_customer'>
-		<div class='col-sm-6'>
-		<div class='form-group row'>
-			<div class='col-md-4'>
-				<label for='email_customer'>EMAIL</label>
-			</div>
-			<div class='col-md-8'>
-				<input type='email' class='form-control' id='email_customer' required name='email_customer' value="<?= $head->email_customer ?>">
-			</div>
-		</div>
-		</div>
-		<div class='col-sm-6'>
-			<div class='form-group row'>
-				<div class='col-md-4'>
-					<label for='id_category_supplier'>PIC CUSTOMER</label>
-				</div>
-				<div class='col-md-8'>
-				<input type='text' class='form-control' id='pic_customer' required name='pic_customer' value="<?= $head->pic_customer ?>">
-				</div>
-			</div>
-		</div>
-		</div>
-		</div>
-		</div>
-			<div class="col-sm-12">
-		<div class="form-group row" >
-		<div class="col-md-12" id='data_customer'>
-		<div class='col-sm-6'>
-		<div class='form-group row'>
-			<div class='col-md-4'>
-				<label for='email_customer'>Berat Maksimum/palet</label>
-			</div>
-			<div class='col-md-8'>
-				<input type='nember' class='form-control' id='berat_palet' required name='berat_palet' value="<?= $head->berat_palet ?>">
-			</div>
-		</div>
-		</div>
-		<div class='col-sm-6'>
-			<div class='form-group row'>
-				<div class='col-md-4'>
-					<label for='id_category_supplier'>Tinggi Maksimum/palet</label>
-				</div>
-				<div class='col-md-8'>
-					<input type='number' class='form-control' id='tinggi_palet' required name='tinggi_palet' value="<?= $head->tinggi_palet ?>">
-				</div>
-			</div>
-		</div>
-		</div>
-		</div>
-		</div>
-		<div class="col-sm-12">
-		<div class="form-group row" >
-		<div class="col-md-12" id='data_customer'>
-		<div class='col-sm-6'>
-		<div class='form-group row'>
-			<div class='col-md-4'>
-				<label for='doc_crcl'>Upload Document CRCL</label>
-			</div>
-			<div class='col-md-8'>
-				<input type='file' class='form-control' id='doc_crcl' required name='doc_crcl' >
-			</div>
-		</div>
-		</div>
-		<div class='col-sm-6' hidden>
-			<div class='form-group row'>
-				<div class='col-md-4'>
-					<label for='id_category_supplier'></label>
-				</div>
-				<div class='col-md-8'>
-					<input type='number' class='form-control' id='tinggi_palet2' required name='tinggi_palet2' >
-				</div>
-			</div>
-		</div>
-		</div>
-		</div>
-		</div>
-			</div>
-			<center>
-		<button type="submit" class="btn btn-success btn-sm" name="save" id="simpan-com"><i class="fa fa-save"></i>Simpan</button>
-			</center>
-				 </div>
-			</div>
-		</form>		  
+		</form>
 	</div>
-</div>	
-	
-				  
-				  
-				  
+</div>
+
+
+
+
 <script type="text/javascript">
 	//$('#input-kendaraan').hide();
-	var base_url			= '<?php echo base_url(); ?>';
-	var active_controller	= '<?php echo($this->uri->segment(1)); ?>';
-	$(document).ready(function(){
+	var base_url = '<?php echo base_url(); ?>';
+	var active_controller = '<?php echo ($this->uri->segment(1)); ?>';
+	$(document).ready(function() {
 		$('.select').select2({
 			width: '100%'
 		});
-			var max_fields2      = 10; //maximum input boxes allowed
-			var wrapper2         = $(".input_fields_wrap2"); //Fields wrapper
-			var add_button2      = $(".add_field_button2"); //Add button ID			
-	$('#simpan-com').click(function(e){
+		var max_fields2 = 10; //maximum input boxes allowed
+		var wrapper2 = $(".input_fields_wrap2"); //Fields wrapper
+		var add_button2 = $(".add_field_button2"); //Add button ID			
+		$('#simpan-com').click(function(e) {
 			e.preventDefault();
-			var deskripsi	= $('#deskripsi').val();
-			var image	= $('#image').val();
-			var idtype	= $('#inventory_1').val();
-			
+			var deskripsi = $('#deskripsi').val();
+			var image = $('#image').val();
+			var idtype = $('#inventory_1').val();
+
 			var data, xhr;
 			swal({
-				  title: "Are you sure?",
-				  text: "You will not be able to process again this data!",
-				  type: "warning",
-				  showCancelButton: true,
-				  confirmButtonClass: "btn-danger",
-				  confirmButtonText: "Yes, Process it!",
-				  cancelButtonText: "No, cancel process!",
-				  closeOnConfirm: true,
-				  closeOnCancel: false
+					title: "Are you sure?",
+					text: "You will not be able to process again this data!",
+					type: "warning",
+					showCancelButton: true,
+					confirmButtonClass: "btn-danger",
+					confirmButtonText: "Yes, Process it!",
+					cancelButtonText: "No, cancel process!",
+					closeOnConfirm: true,
+					closeOnCancel: false
 				},
 				function(isConfirm) {
-				  if (isConfirm) {
-						var formData 	=new FormData($('#data-form')[0]);
-						var baseurl=siteurl+'transaksi_inquiry/SaveEditInquery';
+					if (isConfirm) {
+						var formData = new FormData($('#data-form')[0]);
+						var baseurl = siteurl + 'transaksi_inquiry/SaveEditInquery';
 						$.ajax({
-							url			: baseurl,
-							type		: "POST",
-							data		: formData,
-							cache		: false,
-							dataType	: 'json',
-							processData	: false, 
-							contentType	: false,				
-							success		: function(data){								
-								if(data.status == 1){											
+							url: baseurl,
+							type: "POST",
+							data: formData,
+							cache: false,
+							dataType: 'json',
+							processData: false,
+							contentType: false,
+							success: function(data) {
+								if (data.status == 1) {
 									swal({
-										  title	: "Save Success!",
-										  text	: data.pesan,
-										  type	: "success",
-										  timer	: 7000,
-										  showCancelButton	: false,
-										  showConfirmButton	: false,
-										  allowOutsideClick	: false
-										});
-									window.location.href = base_url + active_controller +'/detail/'+data.code;
-								}else{
-									
-									if(data.status == 2){
+										title: "Save Success!",
+										text: data.pesan,
+										type: "success",
+										timer: 7000,
+										showCancelButton: false,
+										showConfirmButton: false,
+										allowOutsideClick: false
+									});
+									window.location.href = base_url + active_controller + '/detail/' + data.code;
+								} else {
+
+									if (data.status == 2) {
 										swal({
-										  title	: "Save Failed!",
-										  text	: data.pesan,
-										  type	: "warning",
-										  timer	: 7000,
-										  showCancelButton	: false,
-										  showConfirmButton	: false,
-										  allowOutsideClick	: false
+											title: "Save Failed!",
+											text: data.pesan,
+											type: "warning",
+											timer: 7000,
+											showCancelButton: false,
+											showConfirmButton: false,
+											allowOutsideClick: false
 										});
-									}else{
+									} else {
 										swal({
-										  title	: "Save Failed!",
-										  text	: data.pesan,
-										  type	: "warning",
-										  timer	: 7000,
-										  showCancelButton	: false,
-										  showConfirmButton	: false,
-										  allowOutsideClick	: false
+											title: "Save Failed!",
+											text: data.pesan,
+											type: "warning",
+											timer: 7000,
+											showCancelButton: false,
+											showConfirmButton: false,
+											allowOutsideClick: false
 										});
 									}
-									
+
 								}
 							},
 							error: function() {
-								
+
 								swal({
-								  title				: "Error Message !",
-								  text				: 'An Error Occured During Process. Please try again..',						
-								  type				: "warning",								  
-								  timer				: 7000,
-								  showCancelButton	: false,
-								  showConfirmButton	: false,
-								  allowOutsideClick	: false
+									title: "Error Message !",
+									text: 'An Error Occured During Process. Please try again..',
+									type: "warning",
+									timer: 7000,
+									showCancelButton: false,
+									showConfirmButton: false,
+									allowOutsideClick: false
 								});
 							}
 						});
-				  } else {
-					swal("Cancelled", "Data can be process again :)", "error");
-					return false;
-				  }
-			});
+					} else {
+						swal("Cancelled", "Data can be process again :)", "error");
+						return false;
+					}
+				});
 		});
-		
-});
-	function get_customer(){
-        var id_customer=$("#id_customer").val();
-		 $.ajax({
-            type:"GET",
-            url:siteurl+'transaksi_inquiry/getcustomer',
-            data:"id_customer="+id_customer,
-            success:function(html){
-               $("#data_customer").html(html);
-			   $('.select').select2({
+
+	});
+
+	function get_customer() {
+		var id_customer = $("#id_customer").val();
+		$.ajax({
+			type: "GET",
+			url: siteurl + 'transaksi_inquiry/getcustomer',
+			data: "id_customer=" + id_customer,
+			success: function(html) {
+				$("#data_customer").html(html);
+				$('.select').select2({
 					width: '100%'
 				});
-            }
-        });
-    }
-function DelItem(id){
-		$('#data_barang #tr_'+id).remove();
-		
+			}
+		});
 	}
-	
-	
-	
+
+	function DelItem(id) {
+		$('#data_barang #tr_' + id).remove();
+
+	}
 </script>
