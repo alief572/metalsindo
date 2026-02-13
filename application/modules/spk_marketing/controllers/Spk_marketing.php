2c62fd918db62e442c053c95dfe078feda2cb837
alief <aliefdaffa572@gmail.com>
2026-02-13 13:59:54 +0700

    Perbaikan - SPK Marketing

--- application/modules/spk_marketing/controllers/Spk_marketing.php
+++ application/modules/spk_marketing/controllers/Spk_marketing.php
@@ -687,6 +687,7 @@ class Spk_marketing extends Admin_Controller
 			<th><input type='text' class='form-control' value='$dt->nama3|$dt->maker' readonly id='dp_noalloy_$loop' required name='dp[$loop][noalloy]'></th>
 			<th><input type='text' class='form-control' value='$dt->thickness' readonly id='dp_thickness_$loop' required name='dp[$loop][thickness]'></th>
 			<th><input type='text' class='form-control' value='$dt->width' id='dp_width_$loop' required name='dp[$loop][width]'></th>
+			<th><input type='text' class='form-control' value='$dt->length' id='dp_length_$loop' required name='dp[$loop][length]'></th>
 			<th id='part_number'><input type='text' class='form-control' value='$dt->lotno' readonly id='dp_part_number_$loop' required name='dp[$loop][part_number]'></th>
 			<th><input type='text' class='form-control' value='$dt->harga_penawaran_cust' readonly id='dp_hgpenwaran_$loop' required name='dp[$loop][hgpenaaran]'></th>
 			<th><input type='text' class='form-control' value='$dt->harga_penawaran_cust' onkeyup='return AksiDetail($loop);' id='dp_hgdeal_$loop' required name='dp[$loop][hgdeal]'></th>
