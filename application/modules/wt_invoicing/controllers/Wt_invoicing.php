cbed23a0086eb28fa30805b2a3cfe89d720a2b75
Alief Daffa <aliefdaffa572@gmail.com>
2026-04-15 14:58:21 +0700

    Perbaikan - E-Faktur
    Fix: Inbox E-Faktur diurutkan dari invoice yang paling baru

--- application/modules/wt_invoicing/controllers/Wt_invoicing.php
+++ application/modules/wt_invoicing/controllers/Wt_invoicing.php
@@ -2944,7 +2944,7 @@ class Wt_invoicing extends Admin_Controller
 
 			$dataFaktur = [
 				$itemRowIndex,
-				$tanggal_faktur_formatted,
+				'`' . $tanggal_faktur_formatted,
 				"Normal",
 				"",
 				"",
