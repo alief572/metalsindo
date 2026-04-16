8425d4f0b58c482a09a1ad1c04310a5e7a73e689
Alief Daffa <aliefdaffa572@gmail.com>
2026-04-16 13:21:10 +0700

    Perbaikan - Excel Coretax

--- application/modules/wt_invoicing/controllers/Wt_invoicing.php
+++ application/modules/wt_invoicing/controllers/Wt_invoicing.php
@@ -2932,7 +2932,7 @@ class Wt_invoicing extends Admin_Controller
 
 		foreach ($invoices_data as $invoice) {
 
-			$tanggal_faktur_formatted = date('j/n/Y', strtotime($invoice['tanggal_invoice']));
+			$tanggal_faktur_formatted = floor(PHPExcel_Shared_Date::PHPToExcel(strtotime(date('Y-m-d', strtotime($invoice['tanggal_invoice'])))));
 			$NPWP = preg_replace("/[^0-9]/", "", $invoice['npwp']);
 			if (strlen($NPWP) < 16) {
 				$NPWP = str_pad($NPWP, 16, '0', STR_PAD_LEFT);
@@ -3264,7 +3264,7 @@ class Wt_invoicing extends Admin_Controller
 
 		foreach ($invoices_data as $invoice) {
 
-			$tanggal_faktur_formatted = date('j/n/Y', strtotime($invoice['tanggal_invoice']));
+			$tanggal_faktur_formatted = PHPExcel_Shared_Date::PHPToExcel(strtotime(date('Y-m-d', strtotime($invoice['tanggal_invoice']))));
 			$NPWP = preg_replace("/[^0-9]/", "", $invoice['npwp']);
 			if (strlen($NPWP) < 16) {
 				$NPWP = str_pad($NPWP, 16, '0', STR_PAD_LEFT);
