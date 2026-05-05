c0c80e4059c586f8d446660c97c8bf85dcec9edb
alief572 <146901652+alief572@users.noreply.github.com>
2026-05-05 14:23:07 +0700

    Fix swapped width/weight input ids/names
    Correct input id and name attributes in CariW1material and CariW2material so width values use id='used_width_$loop' and name='dt[$loop][width]' and weight values use id='used_weight_$loop' and name='dt[$loop][weight]'. Applied across multiple SPK controllers (spk_aktual/spk_produksi, spk_aktual2-5, spk_aktual_booking, spk_aktual_sheet, spk_aktual_slitting) to fix mismatched form fields.

--- application/modules/spk_aktual_booking/controllers/Spk_aktual_booking.php
+++ application/modules/spk_aktual_booking/controllers/Spk_aktual_booking.php
@@ -954,7 +954,7 @@ function CariW1material()
 		$id_marketing=$_GET['id_marketing'];
 		$dbstocking	= $this->db->query("SELECT * FROM dt_spkmarketing WHERE id_dt_spkmarketing = '$id_marketing' ")->result();
 		$id_customerr = $dbstocking[0]->width;
-		echo "<input type='text' class='form-control'  value='$id_customerr'  readonly	id='used_weight_$loop' required name='dt[$loop][weight]'>";
+		echo "<input type='text' class='form-control'  value='$id_customerr'  readonly	id='used_width_$loop' required name='dt[$loop][width]'>";
 	}
 function CariW2material()
     {
@@ -962,7 +962,7 @@ function CariW2material()
 		$id_marketing=$_GET['id_marketing'];
 		$dbstocking	= $this->db->query("SELECT * FROM dt_spkmarketing WHERE id_dt_spkmarketing = '$id_marketing' ")->result();
 		$id_customerr = $dbstocking[0]->weight;
-		echo "<input type='text' class='form-control'  value='$id_customerr'  readonly	id='used_width_$loop' required name='dt[$loop][width]'>";
+		echo "<input type='text' class='form-control'  value='$id_customerr'  readonly	id='used_weight_$loop' required name='dt[$loop][weight]'>";
 	}
 function CariTW2material()
     {
