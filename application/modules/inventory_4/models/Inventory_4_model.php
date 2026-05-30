a5dfac7e7ec9fd61f8100c9f9ee4c66c5c6c0faa
alief572 <146901652+alief572@users.noreply.github.com>
2026-05-30 22:18:58 +0700

    Fix: Master Material
    Untuk table di inbox, load data nya diubah menjadi datatable serverside

--- application/modules/inventory_4/models/Inventory_4_model.php
+++ application/modules/inventory_4/models/Inventory_4_model.php
@@ -241,4 +241,71 @@ class Inventory_4_model extends BF_Model
 		$query = $this->db->get();
 		return $query->result();
 	}
+
+	// Server Side Processing
+	private function _get_datatables_query($id_bentuk)
+	{
+		$this->db->select('a.*, b.nama as nama_type, c.nama as nama_category1, d.nama as nama_category2');
+		$this->db->from('ms_inventory_category3 a');
+		$this->db->join('ms_inventory_type b', 'b.id_type=a.id_type', 'left');
+		$this->db->join('ms_inventory_category1 c', 'c.id_category1 =a.id_category1', 'left');
+		$this->db->join('ms_inventory_category2 d', 'd.id_category2 =a.id_category2', 'left');
+		$this->db->where('a.deleted', '0');
+		$this->db->where('a.id_bentuk', $id_bentuk);
+
+		$i = 0;
+
+		if ($id_bentuk == 'B2000001') {
+			$column_search = array('a.id_category3', 'b.nama', 'c.nama', 'd.nama', 'a.nama', 'a.spek', 'a.thickness', 'a.hardness', 'a.density', 'a.maker', 'a.negara');
+			$column_order = array(null, 'a.id_category3', 'b.nama', 'c.nama', 'd.nama', 'a.nama', 'a.nama', 'a.spek', 'a.thickness', 'a.hardness', 'a.density', 'a.maker', 'a.negara', 'a.aktif', null);
+		} else {
+			$column_search = array('a.id_category3', 'b.nama', 'c.nama', 'd.nama', 'a.nama', 'a.hardness');
+			$column_order = array(null, 'a.id_category3', 'b.nama', 'c.nama', 'd.nama', 'a.nama', 'a.nama', null, 'a.aktif', null);
+		}
+
+		foreach ($column_search as $item) {
+			if ($_POST['search']['value']) {
+				if ($i === 0) {
+					$this->db->group_start();
+					$this->db->like($item, $_POST['search']['value']);
+				} else {
+					$this->db->or_like($item, $_POST['search']['value']);
+				}
+				if (count($column_search) - 1 == $i)
+					$this->db->group_end();
+			}
+			$i++;
+		}
+
+		if (isset($_POST['order'])) {
+			$this->db->order_by($column_order[$_POST['order']['0']['column']], $_POST['order']['0']['dir']);
+		} else if (isset($this->order)) {
+			$order = $this->order;
+			$this->db->order_by(key($order), $order[key($order)]);
+		}
+	}
+
+	function get_datatables($id_bentuk)
+	{
+		$this->_get_datatables_query($id_bentuk);
+		if ($_POST['length'] != -1)
+			$this->db->limit($_POST['length'], $_POST['start']);
+		$query = $this->db->get();
+		return $query->result();
+	}
+
+	function count_filtered($id_bentuk)
+	{
+		$this->_get_datatables_query($id_bentuk);
+		$query = $this->db->get();
+		return $query->num_rows();
+	}
+
+	public function count_alls($id_bentuk)
+	{
+		$this->db->from('ms_inventory_category3');
+		$this->db->where('deleted', '0');
+		$this->db->where('id_bentuk', $id_bentuk);
+		return $this->db->count_all_results();
+	}
 }
