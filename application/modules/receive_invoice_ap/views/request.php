39b383f1f8d3ed2705c419aa60208b3e42c7980c
alief572 <146901652+alief572@users.noreply.github.com>
2026-07-07 11:57:51 +0700

    Fix: Receive Invoice AP

--- application/modules/receive_invoice_ap/views/request.php
+++ application/modules/receive_invoice_ap/views/request.php
@@ -2,6 +2,7 @@
 <div class="box box-primary">
     <div class="box-body">
         <form id="data-form" method="post">
+            <input type="hidden" id="id_suplier_modal" value="<?= $id_suplier ?>">
             <div class="form-group row">
                 <table class="table table-bordered" width="100%" id="list_item_stokk">
                     <thead>
@@ -15,42 +16,6 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php
-                        $no = 0;
-                        foreach ($results as $item) {
-
-                            $no++;
-
-                            $no_po = (!empty($list_no_po[$item->id_incoming])) ? $list_no_po[$item->id_incoming] : '';
-
-                            $total_incoming = 0;
-
-                            $this->db->select('b.width_recive, a.hargasatuan');
-                            $this->db->from('dt_trans_po a');
-                            $this->db->join('dt_incoming b', 'b.id_dt_po = a.id_dt_po AND b.id_material = a.idmaterial');
-                            $this->db->where('b.id_incoming', $item->id_incoming);
-                            $get_total_incoming = $this->db->get()->result();
-
-                            foreach ($get_total_incoming as $item_incoming) {
-                                $total_incoming += ($item_incoming->hargasatuan * $item_incoming->width_recive);
-                            }
-
-                            echo '<tr>';
-
-                            echo '<td class="text-center">' . $item->id_incoming . '</td>';
-                            echo '<td class="text-center">' . $no_po . '</td>';
-                            echo '<td class="text-center">' . date('d F Y', strtotime($item->tanggal)) . '</td>';
-                            echo '<td class="text-center">' . $item->name_suplier . '</td>';
-                            echo '<td class="text-right">' . number_format($total_incoming, 2) . '</td>';
-                            echo '<td class="text-center">';
-                            echo '<button type="button" class="btn btn-sm btn-warning add_incoming add_incoming_' . $no . '" data-id_incoming="' . $item->id_incoming . '" data-no_po="' . $no_po . '" data-id_suplier="'.$item->id_suplier.'" data-name_suplier="' . $item->name_suplier . '" data-nilai="' . $total_incoming . '" data-tanggal_incoming="'.$item->tanggal.'" data-no="' . $no . '">';
-                            echo '<i class="fa fa-plus"></i> Add';
-                            echo '</button>';
-                            echo '</td>';
-
-                            echo '</tr>';
-                        }
-                        ?>
                     </tbody>
                 </table>
             </div>
@@ -60,6 +25,24 @@
 <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
 <script>
     $(document).ready(function() {
-        $('#list_item_stokk').DataTable();
+        $('#list_item_stokk').DataTable({
+            "processing": true,
+            "serverSide": true,
+            "ajax": {
+                "url": siteurl + active_controller + 'server_side_request',
+                "type": "POST",
+                "data": function(d) {
+                    d.id_suplier = $('#id_suplier_modal').val();
+                }
+            },
+            "columns": [
+                {"data": 0},
+                {"data": 1},
+                {"data": 2},
+                {"data": 3},
+                {"data": 4},
+                {"data": 5, "orderable": false}
+            ]
+        });
     });
 </script>
---
+++ application/modules/receive_invoice_ap/views/testing.php
@@ -0,0 +1,14 @@
+<?php
+$no = 0;
+$total = 0;
+foreach ($detail as $item_detail) :
+    $no++;
+
+    $harga_per_sheet = ($item_detail->hargasatuan * $item_detail->total_weight);
+
+    echo $no . '. ' . $item_detail->lotno . ' - ' . $item_detail->qty_sheet . ' - ' . $harga_per_sheet . ' - ' . ($item_detail->qty_sheet * $harga_per_sheet) . '<br>';
+
+    $total += ($item_detail->qty_sheet * $harga_per_sheet);
+endforeach;
+
+echo "TOTAL : Rp. " . number_format($total, 2);
